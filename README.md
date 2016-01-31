c5dbc0e18aeb03e28b2e05d3a3c58433c6e4a1a1
Jozef Izso <jozef.izso@gmail.com>
2016-01-31 23:24:16 +0100

    Initial project files.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Superfatura API Client
+
+> .NET client library to access Superfaktura API
+
+
+## Licencia
+
+Copyright (c) 2016 Jozef Izso, [MIT License](LICENSE)
---
+++ Superfaktura/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General information about OutlookPlugin assembly.
+[assembly: AssemblyTitle("Superfaktura API")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Superfaktura API")]
+[assembly: AssemblyCopyright("Copyright (c) 2016 Jozef Izso")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Configure COM visibility and typelib identifier.
+[assembly: ComVisible(false)]
+[assembly: Guid("19237fae-5d16-4fb2-b7bc-0d6bb2cb48e1")]
